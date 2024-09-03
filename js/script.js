b385d11c8404c998a238cbcd836055f09edf2e5c
Alison Gonzalez <168144637+VioletDarklight@users.noreply.github.com>
2024-09-03 00:45:21 -0300

    Se filtró los elementos que son strings y se ordenaron alfabeticamente

--- js/script.js
+++ js/script.js
@@ -27,16 +27,24 @@ const strangeArray = [
 // Función que recibe por parámetro un array y muestra sus elementos en pantalla
 function showList(array) {
   const container = document.getElementById("list");
-  container.innerHTML = "";
+  // container.innerHTML = "";
   // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
   array.forEach((element) => {
-    const li = document.createElement("li");
+
+    /* const li = document.createElement("li");
     li.appendChild(document.createTextNode(element));
-    container.appendChild(li);
+    container.appendChild(li); */
+
+    // ALT + 96 => `
+    // Para renderizar una variable se necesita usar ${NOMBREVARIABLE}
+    container.innerHTML += `<li>${element}</li>`;
   });
 }
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  // Escribe tu solución aquí
-  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+document.addEventListener("DOMContentLoaded", () => {
+  let ArrayFiltrado = strangeArray.filter((element) => {
+    return typeof element === "string";
+  }).sort((a, b) => a.localeCompare(b));
+
+  showList(ArrayFiltrado);
 });
